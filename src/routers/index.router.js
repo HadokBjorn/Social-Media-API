cfa08bdc9f42bd5bc517ae55a3f7d2ae13fb1f5a
Tiago <tiagoalex0202@gmail.com>
2023-06-05 09:38:36 -0300

    Correções no index routes

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import usersRouter from "./users.router.js";
+import likesRouter from "./likes.router.js";
 import searchRouter from "./search.routes.js";
 
 const routers = Router();
 
 routers.use(usersRouter);
-routers.use(searchRouter)
+routers.use(likesRouter);
+routers.use(searchRouter);
+
 
 export default routers;
