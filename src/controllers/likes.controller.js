8671601c33829c24ca2130806bc5a11db5d9eee0
Pedro <pedropiacentini@usp.br>
2023-06-05 00:11:39 -0300

    feat: numero de likes

--- src/controllers/likes.controller.js
+++ src/controllers/likes.controller.js
@@ -1,7 +1,35 @@
-import { likeDB } from "../repositories/likes.repository";
+import { getLikesDB, likeDB } from "../repositories/likes.repository.js";
 
 export async function like(req, res) {
     const postId = req.params;
     const userId = res.locals.user.id;
-    await likeDB(postId, userId);
+
+    try {
+        await likeDB(postId, userId);
+        res.sendStatus(201);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
+export async function getLikes(req, res) {
+    const postId = req.params;
+
+    try {
+        const result = await getLikesDB(postId, userId);
+        if (result.rowCount > 0) {
+            const { post_id, num_likes } = result.rows[0];
+
+            res.send({
+                postId: post_id,
+                numLIkes: num_likes,
+                lastLiker: result.rows.map(liker => liker.last_liker)
+            });
+        }
+
+        res.send({ postId, numLIkes: 0, lastLiker: [] });
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+
 }
--- src/repositories/likes.repository.js
+++ src/repositories/likes.repository.js
@@ -6,3 +6,18 @@ export function likeDB(userId, postId) {
         postId
     ]);
 }
+
+export function getLikesDB(postId) {
+    return db.query(`
+    SELECT id AS post_id, 
+    (SELECT COUNT(*) FROM likes WHERE post_id = posts.id) AS num_likes,
+        users.username AS last_liker
+    FROM posts
+    LEFT JOIN likes ON posts.id = likes.post_id
+    LEFT JOIN users ON likes.user_id = users.id
+    WHERE posts.id = <post_id>
+    ORDER BY likes.created_at DESC
+    LIMIT 2;`, [
+        postId
+    ]);
+}
