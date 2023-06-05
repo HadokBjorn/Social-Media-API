0226bba6d426f017eddcf73f70d31c74575274eb
Tiago <tiagoalex0202@gmail.com>
2023-06-05 09:41:07 -0300

    .

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -1,13 +1,11 @@
 import { Router } from "express";
 import usersRouter from "./users.router.js";
 import likesRouter from "./likes.router.js";
-import searchRouter from "./search.routes.js";
 
 const routers = Router();
 
 routers.use(usersRouter);
 routers.use(likesRouter);
-routers.use(searchRouter);
 
 
 export default routers;
