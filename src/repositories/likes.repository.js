64d3ce31c53f6ad10b8422a61992977314856768
Pedro <pedropiacentini@usp.br>
2023-06-05 09:49:40 -0300

    fix: tirar like

--- src/repositories/likes.repository.js
+++ src/repositories/likes.repository.js
@@ -1,6 +1,7 @@
 import { db } from "../database/database.connections.js";
 
 export function likeDB(userId, postId) {
+    console.log(userId, postId)
     return db.query(`INSERT INTO likes (post_id, user_id) VALUES ($1,$2)`, [
         userId,
         postId
@@ -8,6 +9,8 @@ export function likeDB(userId, postId) {
 }
 
 export function unLikeDB(userId, postId) {
+    console.log(userId, postId)
+
     return db.query(`
     DELETE FROM likes
         WHERE post_id = $1
@@ -20,13 +23,13 @@ export function unLikeDB(userId, postId) {
 
 export function getLikesDB(postId) {
     return db.query(`
-    SELECT id AS post_id, 
+    SELECT posts.id AS post_id, 
     (SELECT COUNT(*) FROM likes WHERE post_id = posts.id) AS num_likes,
         users.username AS last_liker
     FROM posts
     LEFT JOIN likes ON posts.id = likes.post_id
     LEFT JOIN users ON likes.user_id = users.id
-    WHERE posts.id = <post_id>
+    WHERE posts.id = $1
     ORDER BY likes.created_at DESC
     LIMIT 2;`, [
         postId
--- src/routers/likes.router.js
+++ src/routers/likes.router.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { authorization } from "../middlewares/auth.middleware.js";
-import { like, unLike } from "../controllers/likes.controller.js";
+import { getLikes, like, unLike } from "../controllers/likes.controller.js";
 
 const likesRouter = Router();
 
 likesRouter.post("/posts/:id/like", authorization, like);
 likesRouter.delete("/posts/:id/like", authorization, unLike);
-likesRouter.get("/posts/:id/likes");
+likesRouter.get("/posts/:id/likes", getLikes);
 
 export default likesRouter;
