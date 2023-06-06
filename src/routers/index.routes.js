126eba0ee6098716095580a394bef078a774484b
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-06-05 21:10:03 -0500

    fix: update routes name

--- src/routers/index.routes.js
+++ src/routers/index.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import usersRouter from "./users.router.js";
-import likesRouter from "./likes.router.js";
+import usersRouter from "./users.routes.js";
+import likesRouter from "./likes.routes.js";
 import searchRouter from "./search.routes.js";
 
 const routers = Router();
 
 routers.use(usersRouter);
 routers.use(likesRouter);
-routers.use(searchRouter)
+routers.use(searchRouter);
 
 export default routers;
