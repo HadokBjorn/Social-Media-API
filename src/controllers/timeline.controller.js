71166a68fa3400e00340a0c1c93965489b5f6421
klaus Kindermann <klauskindermann94@gmail.com>
2023-06-06 00:12:58 -0300

    adicionando a timeline

---
+++ src/controllers/timeline.controller.js
@@ -0,0 +1,30 @@
+import { db } from '../database/database.connections.js'
+
+export async function getAllPosts(req, res) {
+    try {
+        const post = await db.query('SELECT * FROM posts ORDER BY id DESC')
+
+        res.send(post.rows)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
+
+export async function createPost(req, res) {
+    const { link, description } = req.body
+    const { id } = res.locals.user;
+
+    try {
+        await db.query(`
+        INSERT INTO posts (user_id, link, description)
+        VALUES ($1, $2, $3)
+      `, [Number(id), link, description])
+
+        res.sendStatus(201)
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error.message)
+    }
+
+}
