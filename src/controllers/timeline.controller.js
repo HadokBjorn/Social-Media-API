9543917795419a7e990e148acbe1b5c154948024
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-06-08 02:54:30 -0500

    feat: get posts

--- src/controllers/timeline.controller.js
+++ src/controllers/timeline.controller.js
@@ -1,30 +1,33 @@
-import { db } from '../database/database.connections.js'
+import { db } from "../database/database.connections.js";
 
 export async function getAllPosts(req, res) {
-    try {
-        const post = await db.query('SELECT * FROM posts ORDER BY id DESC')
+	try {
+		const post = await db.query(
+			`SELECT users.username, users.image, posts.* FROM posts JOIN users ON users.id=posts.user_id ORDER BY id DESC`
+		);
 
-        res.send(post.rows)
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
+		res.send(post.rows);
+	} catch (error) {
+		res.status(500).send(error.message);
+	}
 }
 
 export async function createPost(req, res) {
-    const { link, description } = req.body
-    const { id } = res.locals.user;
+	const { link, description } = req.body;
+	const { id } = res.locals.user;
 
-    try {
-        await db.query(`
+	try {
+		await db.query(
+			`
         INSERT INTO posts (user_id, link, description)
         VALUES ($1, $2, $3)
-      `, [Number(id), link, description])
+      `,
+			[Number(id), link, description]
+		);
 
-        res.sendStatus(201)
-
-    } catch (error) {
-        console.log(error)
-        res.status(500).send(error.message)
-    }
-
+		res.sendStatus(201);
+	} catch (error) {
+		console.log(error);
+		res.status(500).send(error.message);
+	}
 }
