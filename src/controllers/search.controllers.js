877812810cf35baf4b3ab291b62caca2cec50aee
Tiago <tiagoalex0202@gmail.com>
2023-06-09 12:29:14 -0300

    .

--- src/controllers/search.controllers.js
+++ src/controllers/search.controllers.js
@@ -27,6 +27,7 @@ export async function UserSearch(req, res) {
 
         for (let i=0; i < compatibleUsers.length; i++){
             let userId = compatibleUsers[i].id;
+            res.send(compatibleUsers[i].email)
             for(let j=0; j < followedIds; j++){
                 if(userId === followedIds[j]){
                     orderedU.push(compatibleUsers[i])
