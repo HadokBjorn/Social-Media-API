dc534b95fb1ae092847b780b0341de1ef0e1bfd1
Esaú Bandeira <113884763+HadokBjorn@users.noreply.github.com>
2023-06-09 15:52:50 -0500

    Update query comments

--- src/repositories/comments.repository.js
+++ src/repositories/comments.repository.js
@@ -11,15 +11,27 @@ export function createCommentDB(body) {
 	);
 }
 
-export function getCommentsDB(id) {
+export function getCommentsDB(id, userId) {
 	return db.query(
-		`
-    SELECT users.username, users.image, comments.*
-    FROM users
-    JOIN comments ON comments.user_id=users.id
-    AND comments.post_id=$1
-    ORDER BY (comments.created_at) DESC
-    `,
-		[Number(id)]
+		`SELECT
+		  users.username,
+		  users.image,
+		  comments.*,
+		  CASE
+		    WHEN followers.follower_id IS NOT NULL THEN TRUE
+		    ELSE FALSE
+		  END AS is_following
+		FROM
+		  users
+		JOIN
+		  comments ON comments.user_id = users.id
+		  AND comments.post_id=$1
+		LEFT JOIN
+		  followers ON followers.follower_id = comments.user_id
+		   AND followers.follower_id = $2
+		ORDER BY
+		  comments.created_at DESC;
+		`,
+		[Number(id), Number(userId)]
 	);
 }
